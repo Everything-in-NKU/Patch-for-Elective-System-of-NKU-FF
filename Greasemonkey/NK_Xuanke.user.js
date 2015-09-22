ba5794adebe4169031f1746d4cb5e264104488c1
JingTao Chen <nankai.codingcat@outlook.com>
2015-09-23 00:43:16 +0800

    Init

---
+++ Greasemonkey/NK_Xuanke.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        NK_Xuanke
+// @namespace   nku
+// @include     http://222.30.32.10/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+function insJS(src, pos) {
+  if ('function' == typeof source) {
+    // Execute this function with no arguments, by adding parentheses.
+    // One set around the function, required for valid syntax, and a
+    // second empty set calls the surrounded function.
+    var source = '(' + src + ')();'
+  }
+  else{
+    var source = src;
+  }
+  var script = document.createElement('script');
+  script.setAttribute('dataTyoe', 'application/javascript');
+  script.textContent = source;
+  pos.appendChild(script);
+}
+window.onload = function () {
+  if (document.getElementsByName('userpwd_text') [0] !== undefined) { //Patch for the Password input
+    document.getElementsByName('userpwd_text') [0].id = 'userpwd_text';
+  }
+  var aa = window.top.document.getElementsByName('leftFrame') [0];
+  if (aa !== undefined) { //patch for the side bar
+    aa = aa.contentDocument;
+    var t = 110;
+    for (var i = 0; i < 21; i++) {
+      var tmp = aa.getElementById('MFX' + i);
+      if (tmp) {
+        tmp.style['visibility'] = 'visible';
+        tmp.style['top'] = t + 'px';
+        t = t + 12;
+      }
+    }
+  }
+  var cc = window.top.document.getElementsByName('mainFrame') [0].contentDocument;
+  cc.onload = function () {
+    insJS('var index = document.getElementsByTagName("input")[1];var operation = document.getElementsByTagName("input")[0];var submit = (function () { document.getElementsByTagName("form")[0].submit(); });', cc)
+  }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+#Patch-for-Elective-System-of-NKU-FF
+
+--------------------------------------------------------
+将[Patch-for-Elective-System-of-NKU](https://github.com/Neon4o4/Patch-for-Elective-System-of-NKU)迁移到火狐上的Project
+
+并不知道要折腾多久，火狐的各种奇怪的逻辑和chrome并不一样呢
+
+要不是那五美刀交不上谁特么用火狐= =谷歌你为什么不和我做朋友QAQ！
